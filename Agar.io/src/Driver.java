176f2e818050435ded0e6a716ff3a6787523230a
eric-foss <70965642+eric-foss@users.noreply.github.com>
2021-01-13 10:41:39 -0800

    Add files via upload

---
+++ Agar.io/src/Driver.java
@@ -0,0 +1,145 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.JFrame;
+
+public class Driver extends JPanel implements MouseListener, ActionListener{
+	
+	
+	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	
+	ArrayList<Food> food = new ArrayList<Food>();
+	
+	Rectangle world = new Rectangle(-500, -500, 2000, 2000);
+	
+	
+	
+	public void paint(Graphics g) {
+		
+		
+		
+		super.paintComponent(g);
+		
+		
+		for (Enemy e: enemies) {
+			e.paint(g);
+		
+		}
+		
+		for(Food e: food) {
+			e.paint(g);
+		}
+		
+		
+		
+		
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).checkBounds();
+		}
+		
+		isColliding();
+		
+		
+	}
+	
+	public static void main(String[] arg) {
+		Driver d = new Driver();
+	}
+	
+	Driver(){
+		JFrame frame = new JFrame("Agar.io");
+		frame.setSize(800, 600);
+		frame.add(this);
+		
+		for (int i =0; i < 50; i++) {
+			enemies.add(new Enemy());
+		}
+		
+		for(int i = 0; i < 30; i++) {
+			food.add(new Food());
+		}
+		
+		
+		Timer t = new Timer(16, this);
+		t.start();
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+	public void isColliding() {
+		int temp = enemies.size();
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(int i = 0; i < temp; i++) {
+			for(int o = 0; o < temp; o++) {
+				if (i == o) {
+					continue;
+				}
+				if ((int) distance(i, o) <= enemies.get(i).getRad() + enemies.get(o).getRad()) {
+					if (enemies.get(i).getRad() >= enemies.get(o).getRad()) {
+						enemies.get(i).setArea(enemies.get(i).getArea()+enemies.get(o).getArea());
+						enemies.remove(o);
+						temp--;
+					}
+					
+					else {
+						enemies.get(i).setArea(enemies.get(i).getArea()+enemies.get(o).getArea());
+						enemies.remove(i);
+						
+						temp--;
+					}
+				}
+				
+			}
+		}
+	}
+	
+	public double distance(int i, int o) {
+		return Math.sqrt(Math.pow((enemies.get(i).getX() + enemies.get(i).getRad()) - (enemies.get(o).getX() + enemies.get(o).getRad()), 2.0) +
+				Math.pow((enemies.get(i).getY() + enemies.get(i).getRad()) - (enemies.get(o).getY() + enemies.get(o).getRad()), 2.0));
+	}
+	
+	
+	
+	public void mouseClicked(MouseEvent arg0) {
+		
+	}
+	
+	public void mouseEntered(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		repaint();
+		
+	}
+
+}
---
+++ Agar.io/src/Enemy.java
@@ -0,0 +1,113 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Enemy {
+	
+	private int x, y;
+	private int vx, vy;
+	private int vmag;
+	private int theta;
+	private int rad;
+	private int mass;
+	private int area;
+	private Color color;
+	
+	Rectangle world = new Rectangle(-500, -500, 2000, 2000);
+	
+	public Enemy() {
+		
+		rad =  (int) (Math.random()*(60-10+1)+10);
+		area = (int) ((Math.PI)*(Math.pow(rad, 2.0))); 
+		
+		vmag = (int) (8 - area/4000);
+		theta = (int) (Math.random()*(360-0+1)+0);
+		
+		vx = (int) (vmag*(Math.cos(theta)));
+		vy = (int) (vmag*(Math.sin(theta)));
+		
+		
+		
+		
+		
+		
+		x = (int) (Math.random()*801);
+		y = (int) (Math.random()*601);
+		
+		int red = (int)(Math.random()*256);
+		int green = (int)(Math.random()*256);
+		int blue =  (int)(Math.random()*178);
+		color = new Color(red,green,blue);
+		
+		
+		
+		
+		
+	}
+		
+
+	public void checkBounds() {
+		if (x < -500) {
+			vx*= -1;
+			x = -500;
+		}
+		
+		if (x > 1500 - 2*rad) {
+			vx *= -1;
+			x = 1500 - 2*rad;
+		}
+		
+		if (y < -500) {
+			vy *= -1;
+			y = -500;
+		}
+		
+		if (y > 1500 - 2*rad) {
+			vy *= -1;
+			y = 1500 - 2*rad;
+		}
+	}
+	
+	
+	public void collide(Enemy e) {
+		
+	}
+	
+	public void paint(Graphics g) {
+		update();
+		g.setColor(color);
+		g.fillOval(x,  y,  rad*2,  rad*2);
+	}
+	
+	
+	
+	
+	public void update() {
+		x += vx;
+		y += vy;
+	}
+	
+	
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getRad() {
+		return rad;
+	}
+	
+	
+	public int getArea() {
+		return area;
+	}
+	
+	public void setArea(int area) {
+		this.area = area;
+		
+	}
+}
